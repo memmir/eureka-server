2af789250dda5115f1e1d20e2aef802b40318162
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-05-02 11:39:49 +0300

    changed package name

--- src/main/java/com/microshop/stockmanagement/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/microshop/stockmanagement/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.mahmuttech.stockmanagement.eurekaserver;
+package com.microshop.stockmanagement.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/microshop/stockmanagement/eurekaserver/EurekaServerApplicationTests.java
+++ src/test/java/com/microshop/stockmanagement/eurekaserver/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.mahmuttech.stockmanagement.eurekaserver;
+package com.microshop.stockmanagement.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
